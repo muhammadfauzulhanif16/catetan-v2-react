f33b82f0f699c251c729c6bad62129a07a18b022
muhammadfauzulhani16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-05-21 14:38:25 +0700

    test

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box, Flex } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import { NavBar } from './NavBar'
 import { Header } from './Header'
 
 export const Layout = ({ children }) => {
   return (
-    <Flex
-      direction='column'
-      // color='gray.600'
-      h='100vh'
-      pos='fixed'
-      w='full'
-    >
+    <Box w='full'>
       <Header />
 
-      <Box px={[4, 8, 12]} pb={[24, 32, 40]} h='full' overflow='auto'>
+      <Box px={[4, 8, 12]} h='full'>
         {children}
       </Box>
 
       <NavBar />
-    </Flex>
+    </Box>
   )
 }
 
--- src/components/Shelf.jsx
+++ src/components/Shelf.jsx
@@ -4,10 +4,9 @@ import { noteList } from '../data/noteList'
 
 export const Shelf = () => {
   const notes = noteList(50)
-  console.log(notes)
 
   return (
-    <SimpleGrid columns={[1, 2, 4]} gap={4} w='full'>
+    <SimpleGrid columns={[1, 2, 4]} gap={4} w='full' pb={[24, 32, 40]}>
       {notes.map(({ id }, idx) => (
         <GridItem key={idx} bgColor='blue.200' h={16}>
           {id}
