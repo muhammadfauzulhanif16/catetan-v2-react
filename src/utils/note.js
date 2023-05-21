38a9ea2c841bb857ad2dd79db20c1521a4f58fec
muhammadfauzulhani16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-05-21 16:52:43 +0700

    test

--- src/utils/note.js
+++ src/utils/note.js
@@ -1,13 +1,15 @@
 export const titleChange = (e, note, setNote) => {
-  setNote({
-    title: {
-      content: e.target.value,
-      max: 50 - e.target.value.length
-    },
-    body: {
-      content: note.body.content
-    }
-  })
+  if (e.target.value.length <= 50) {
+    setNote({
+      title: {
+        content: e.target.value,
+        max: 50 - e.target.value.length
+      },
+      body: {
+        content: note.body.content
+      }
+    })
 
-  return setNote
+    return setNote
+  }
 }
